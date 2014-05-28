4c5a276271365afbde1243633ee837bbe44e268e
Kostya Sergeev <kostyasergeev93@gmail.com>
2014-05-28 23:52:58 +0300

    first implementation of algorithm

--- injected.js
+++ injected.js
@@ -1,42 +1,57 @@
-alert("injected");
-/*var goods = [].slice.apply(document.getElementsByTagName('a'));
-goods = goods.map(function(element) {
-  // Return an anchor's href attributse, stripping any URL fragment (hash '#').
-  // If the html specifies a relative path, chrome converts it to an absolute
-  // URL.
-  var href = element.href;
-  var hashIndex = href.indexOf('#');
-  if (hashIndex >= 0) {
-    href = href.substr(0, hashIndex);
+(function () {
+  function searchBigElements(minChildrenNumber, element) {
+    var elements = [].slice.apply(element.querySelectorAll("div,tbody,tr,td"));
+    return bigElements = elements.filter(function(element) {
+      if (element.children.length > minChildrenNumber) return element;
+    });
+  } 
+
+  function searchMaxElement(myElement) {
+    var elements = [].slice.apply(myElement.querySelectorAll("div,tbody,tr,td"));
+    var currentMax = elements[0];
+
+    if (typeof currentMax === "undefined") 
+      return 0;
+
+    elements.filter(function(element) {
+      if (element.childElementCount > currentMax.childElementCount && element.parentNode == myElement) {
+          currentMax = element;
+        }
+    });
+    return currentMax.childElementCount;
+  } 
+
+  function searchEpicElement(bigElements) {
+    var epicElements = [];
+    var temp = 0;
+    var index = 0;
+    for (var i = 0; i < bigElements.length; i++) {
+      epicElements.push(searchMaxElement(bigElements[i]))
+    }
+
+    for (var i = 0; i < epicElements.length; i++) {
+      if (epicElements[i] > temp) {temp = epicElements[i]; index = i;}
+    }
+  console.log(epicElements)
+  console.log(index);
+  return index;
   }
-  return href;
-});*/
 
-var elements = document.getElementsByTagName("*");
-var maxElement = elements[0];
-for (i = 0; i < elements.length; i++) {
-	if (maxElement.childNodes.length < elements[i].childNodes.length) {
-			maxElement = elements[i]
-	}
-}
-//console.log(maxElement.childNodes.length);
-//console.log(maxElement);
+  bElements = searchBigElements(15, document);
+  console.log(bElements);
+  var index = searchEpicElement(bElements);
 
-var goods = [].slice.apply(maxElement.getElementsByTagName("a"));
-goods = goods.map(function(element) {
-  // Return an anchor's href attributse, stripping any URL fragment (hash '#').
-  // If the html specifies a relative path, chrome converts it to an absolute
-  // URL.
+  var goods = [].slice.apply(bElements[index].getElementsByTagName("a"));
+  goods = goods.map(function(element) {
   var href = element.href;
-  var hashIndex = href.indexOf('#');
-  if (hashIndex >= 0) {
-    href = href.substr(0, hashIndex);
-  }
-  return href;
-});
+    var hashIndex = href.indexOf('#');
+    if (hashIndex >= 0) {
+      href = href.substr(0, hashIndex);
+    }
+    return href;
+  });
 
-console.log(goods);
+  console.log(goods);
 
-
-chrome.extension.sendRequest(goods);
-//alert("all good");
+  chrome.extension.sendRequest(goods);
+}) ();
