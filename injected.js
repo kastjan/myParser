73c84f2366278b941e44eba80c76296c1a6a8006
Kostya Sergeev <kostyasergeev93@gmail.com>
2014-06-08 17:55:47 +0300

    now exactly all

--- injected.js
+++ injected.js
@@ -4,35 +4,28 @@
 
   Array.prototype.max = function () {return Math.max.apply(Math, this)}
 
-  function searchEpicElement(minChildrenNumber, element) {
-    var elements = [].slice.apply(element.querySelectorAll("div,tbody,tr,td,ul"));
-    bigElements = elements.filter(function(element) {
-      if (element.children.length > minChildrenNumber) return element;
-    });
-    var areas = bigElements.map(function (x) {return x.offsetWidth * x.offsetHeight});
-    return bigElements[areas.indexOf(areas.max())];
+  function searchEpicElement() {
+    var elements = [].slice.apply(document.querySelectorAll("div,tbody,tr,td,ul"));
+    elements = elements.filter(function(x) {if (x.children.length > 10) return x});
+    var areas = elements.map(function (x) {return x.offsetWidth * x.offsetHeight});
+    return elements[areas.indexOf(areas.max())];
   } 
 
-  var epicElement = searchEpicElement(10, document);
-
-  var goods = [].slice.apply(epicElement.getElementsByTagName("img"));
-  goods = goods.map(function(element) {
-  if (element.offsetHeight > 40) var alt = element.alt;
-  if (alt) {
-    while (element.parentNode != epicElement) element = element.parentNode;
-    var str = element.innerHTML;
-    var matches = reg.exec(str);
-    if (matches) {
-      str = str.substring(matches.index, matches.index + 200);
-      matches = regPrice.exec(str);
-        if (matches) alt += ": " + matches[0];
-        else  alt += ": Is not on sale.";
-    }
+  function getGoods(item) {
+    var alt = item.alt;
+    while (item.parentNode != epicElement) item = item.parentNode;
+    var str = item.innerHTML;
+    if (matches = reg.exec(str)) {
+      matches = regPrice.exec(str.substring(matches.index, matches.index + 200));
+      alt += (matches) ? ": " + matches[0] : ": Is not on sale.";
+      }
     return alt;
   }
-  });
 
-  goods = goods.filter( function(x) { if (x) return x});
-  goods[goods.length] = window.location.href;
+  var epicElement = searchEpicElement();
+  var goods = [].slice.apply(epicElement.getElementsByTagName("img"));
+  goods = goods.filter(function(x) {if (x.offsetHeight > 40) return x});
+  goods = goods.map(getGoods);
+  goods.push(window.location.href);
   chrome.extension.sendRequest(goods);
 }) ();
