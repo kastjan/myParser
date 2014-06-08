043eeff2a5ec2a8479df4e9ce1c014486c98fc2b
Kostya Sergeev <kostyasergeev93@gmail.com>
2014-06-08 16:47:45 +0300

    finally

--- injected.js
+++ injected.js
@@ -1,18 +1,15 @@
 (function () {
   var reg = new RegExp("price", "i");
   var regPrice = new RegExp("\\d+(.+)\\d+\\s+(\\S+)", "i");
-  var allgoods = [];
 
-  Array.prototype.max = function () {
-    return Math.max.apply(Math, this);
-  }
+  Array.prototype.max = function () {return Math.max.apply(Math, this)}
 
   function searchEpicElement(minChildrenNumber, element) {
     var elements = [].slice.apply(element.querySelectorAll("div,tbody,tr,td,ul"));
     bigElements = elements.filter(function(element) {
       if (element.children.length > minChildrenNumber) return element;
     });
-    var areas = bigElements.map(function (x) {return x.offsetWidth * x.offsetHeight;});
+    var areas = bigElements.map(function (x) {return x.offsetWidth * x.offsetHeight});
     return bigElements[areas.indexOf(areas.max())];
   } 
 
@@ -21,21 +18,21 @@
   var goods = [].slice.apply(epicElement.getElementsByTagName("img"));
   goods = goods.map(function(element) {
   if (element.offsetHeight > 40) var alt = element.alt;
-  if (typeof alt != "undefined" && alt != "") {
+  if (alt) {
     while (element.parentNode != epicElement) element = element.parentNode;
     var str = element.innerHTML;
     var matches = reg.exec(str);
-    if (matches != null) {
+    if (matches) {
       str = str.substring(matches.index, matches.index + 200);
       matches = regPrice.exec(str);
-        if (matches != null) alt += ": " + matches[0];
+        if (matches) alt += ": " + matches[0];
         else  alt += ": Is not on sale.";
     }
     return alt;
   }
   });
 
-  goods = goods.filter( function(x) { if (typeof x != "undefined") return x;});
+  goods = goods.filter( function(x) { if (x) return x});
   goods[goods.length] = window.location.href;
   chrome.extension.sendRequest(goods);
 }) ();
